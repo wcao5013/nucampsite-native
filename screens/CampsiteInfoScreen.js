8946be86c7963ab31ee5efc414b28ae56fbfde6a
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-09-19 16:44:12 -0400

    message

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,30 +1,37 @@
-import RenderCampsite from '../features/campsites/RenderCampsite';
 import { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import { COMMENTS } from '../shared/comments'
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import RenderCampsite from '../features/campsites/RenderCampsite';
+import { COMMENTS } from '../shared/comments';
 
 const CampsiteInfoScreen = ({ route }) => {
-    const { campsite } = route.params
-    const [comments, setComments] = useState(COMMENTS)
-    
+    const { campsite } = route.params;
+
+    const [comments, setComments] = useState(COMMENTS);
+    const [favorite, setFavorite] = useState(false);
+
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
                 <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
-                <Text style={{ fontSize: 12 }}>{`--${item.author}, ${item.date}`}</Text>
+                <Text style={{ fontSize: 12 }}>
+                    {`-- ${item.author}, ${item.date}`}
+                </Text>
             </View>
-        )
-    }
-    
+        );
+    };
+
     return (
         <FlatList
-            data={
-                comments.filter((comment) => comment.campsiteId === campsite.id)}
+            data={comments.filter(
+                (comment) => comment.campsiteId === campsite.id
+            )}
             renderItem={renderCommentItem}
             keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20}}
-
+            contentContainerStyle={{
+                marginHorizontal: 20,
+                paddingVertical: 20
+            }}
             ListHeaderComponent={
                 <>
                     <RenderCampsite
@@ -32,14 +39,12 @@ const CampsiteInfoScreen = ({ route }) => {
                         isFavorite={favorite}
                         markFavorite={() => setFavorite(true)}
                     />
-                    <Text style={StyleSheet.commentsTitle}>Comments</Text>
+                    <Text style={styles.commentsTitle}>Comments</Text>
                 </>
             }
         />
-    )
+    );
 };
-
-export default CampsiteInfoScreen;
 
 const styles = StyleSheet.create({
     commentsTitle: {
@@ -57,3 +62,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff'
     }
 });
+
+export default CampsiteInfoScreen;
