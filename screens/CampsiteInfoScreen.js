3c7ea5a52d3a338a5ccc0e74c5e8a67f0124cc5f
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-09-19 16:30:06 -0400

    message

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,9 +1,32 @@
 import RenderCampsite from '../features/campsites/RenderCampsite';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { COMMENTS } from '../shared/comments'
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params
+    const [comments, setComments] = useState(COMMENTS)
     
-    return <RenderCampsite campsite={campsite} />;
+    return (
+        <FlatList
+            data={
+                comments.filter((comment) => comment.campsiteId === campsite.id)}
+            renderItem={renderCommentItem}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20}}
+
+            ListHeaderComponent={
+                <>
+                    <RenderCampsite
+                        campsite={campsite}
+                        isFavorite={favorite}
+                        markFavorite={() => setFavorite(true)}
+                    />
+                    <Text style={StyleSheet.commentsTitle}>Comments</Text>
+                </>
+            }
+        />
+    )
 };
 
 export default CampsiteInfoScreen;
