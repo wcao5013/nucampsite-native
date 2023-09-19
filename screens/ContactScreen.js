06ef913cbde8403afcdd122b3c9216716165b732
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-09-19 12:09:38 -0400

    message

---
+++ screens/ContactScreen.js
@@ -0,0 +1,12 @@
+import { ScrollView } from "react-native"
+i
+
+const ContactScreen = () => {
+    return (
+            <ScrollView>
+                
+            </ScrollView>
+        )
+}
+
+export default ContactScreen
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,8 @@ import DirectoryScreen from './DirectoryScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -49,6 +51,26 @@ const DirectoryNavigator = () => {
     );
 };
 
+const AboutNavigator = () => {
+    const Stack = createStackNavigator()
+
+    return (
+        <Stack.Navigator>
+                <Stack.Screen name='About' />
+        </Stack.Navigator>
+    )
+}
+
+const ContactNavigator = () => {
+    const Stack = createStackNavigator()
+
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='Contact' options={{ title: 'Contact Us'}} />
+        </Stack.Navigator>
+    )
+}
+
 const Main = () => {
     return (
         <View
@@ -71,6 +93,15 @@ const Main = () => {
                     name='Directory'
                     component={DirectoryNavigator}
                     options={{ title: 'Directory' }}
+                />
+                <Drawer.Screen
+                    name='About'
+                    component={AboutNavigator}
+                />
+                <Drawer.Screen
+                    name='Contact'
+                    component={ContactNavigator}
+                    options={{title: 'Contact Us'}}
                 />
             </Drawer.Navigator>
         </View>
