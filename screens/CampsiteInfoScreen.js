824b40df33bf5851b905ae1522a4a6048e96a6d1
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-09-19 16:36:26 -0400

    message

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -7,6 +7,16 @@ const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params
     const [comments, setComments] = useState(COMMENTS)
     
+    const renderCommentItem = ({ item }) => {
+        return (
+            <View style={styles.commentItem}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>{`--${item.author}, ${item.date}`}</Text>
+            </View>
+        )
+    }
+    
     return (
         <FlatList
             data={
@@ -30,3 +40,20 @@ const CampsiteInfoScreen = ({ route }) => {
 };
 
 export default CampsiteInfoScreen;
+
+const styles = StyleSheet.create({
+    commentsTitle: {
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#43484D',
+        padding: 10,
+        paddingTop: 30
+    },
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#fff'
+    }
+});
