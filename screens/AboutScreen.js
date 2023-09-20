e2c97ef570c1744bf82e75af9a5fa9eeade122f8
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-09-20 12:38:24 -0400

    message

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,45 +1,78 @@
-import { Text, View, ScrollView } from 'react-native'
-import { Avatar, Card, ListItem } from 'react-native-elements'
-import { useSelector } from 'react-redux'
-import { baseUrl } from '../shared/baseUrl'
+import { ScrollView, Text } from 'react-native';
+import { Avatar, Card, ListItem } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
-const Mission = () => {
+function Mission() {
     return (
-            <Card style={{margin: 10}}>
-                <Card.Title> Our Mission</Card.Title>
-                <Card.Divider />
-                    <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness.</Text>
-                    <Text> We increase access to adventure for the public while promoting safe and respectful use of resources. </Text>
-                    <Text>The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. </Text>
-                    <Text>We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
-            </Card>
-    )
+        <Card>
+            <Card.Title>Our Mission</Card.Title>
+            <Card.Divider />
+            <Text style={{ margin: 10 }}>
+                We present a curated database of the best campsites in the vast
+                woods and backcountry of the World Wide Web Wilderness. We
+                increase access to adventure for the public while promoting safe
+                and respectful use of resources. The expert wilderness trekkers
+                on our staff personally verify each campsite to make sure that
+                they are up to our standards. We also present a platform for
+                campers to share reviews on campsites they have visited with
+                each other.
+            </Text>
+        </Card>
+    );
 }
-const AboutScreen = () => {
-        const partners = useSelector(( state ) => state.partners)
 
+const AboutScreen = () => {
+    const partners = useSelector((state) => state.partners);
+
+    if (partners.isLoading) {
         return (
             <ScrollView>
                 <Mission />
                 <Card>
-                    <Card.Title>Community partners</Card.Title>
+                    <Card.Title>Community Partners</Card.Title>
                     <Card.Divider />
-                    {partners.partnersArray.map((partner) => {
-                        return (
-                            <ListItem key={partner.id}>
-                                <Avatar source={{ uri: baseUrl + partner.image}} rounded />
-                                <ListItem.Content>
-                                    <ListItem.Title>{partner.name}</ListItem.Title>
-                                    <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-                                </ListItem.Content>
-                            </ListItem>
-                        )
-                    })}
+                    <Loading />
                 </Card>
             </ScrollView>
-        )
-}
+        );
+    }
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+    return (
+        <ScrollView>
+            <Mission />
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider />
+                {partners.partnersArray.map((partner) => (
+                    <ListItem key={partner.id}>
+                        <Avatar
+                            rounded
+                            source={{ uri: baseUrl + partner.image }}
+                        />
+                        <ListItem.Content>
+                            <ListItem.Title>{partner.name}</ListItem.Title>
+                            <ListItem.Subtitle>
+                                {partner.description}
+                            </ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                ))}
+            </Card>
+        </ScrollView>
+    );
+};
 
-
-
-export default AboutScreen
+export default AboutScreen;
