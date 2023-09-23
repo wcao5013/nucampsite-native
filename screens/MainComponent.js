4ef5e0513107ea093ddf10a9a6d7befd6c2c03e8
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-09-23 13:26:09 -0400

    message

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -21,6 +21,7 @@ import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from './ReservationScreen';
 import FavoritesScreen from './FavoritesScreen';
+import LoginScreen from './LoginScreen';
 
 
 const Drawer = createDrawerNavigator();
@@ -146,6 +147,29 @@ const FavoritesNavigator = () => {
     );
 };
 
+const LoginNavigator = () => {
+    const Stack = createStackNavigator();
+    
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Login'
+                component={LoginScreen}
+                options={({ navigation }) => ({
+                    headerLeft: () => (
+                        <Icon
+                            name='sign-in'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
+
 const DirectoryNavigator = () => {
     const Stack = createStackNavigator();
     return (
@@ -216,6 +240,24 @@ const Main = () => {
                 drawerContent={CustomDrawerContent}
                 drawerStyle={{ backgroundColor: '#CEC8FF' }}
             >
+                
+                <Drawer.Screen
+                    name='Login'
+                    component={LoginNavigator}
+                    options={{
+
+                        drawerIcon: ({ color }) => (
+                            <Icon
+                                name='sign-in'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
+
                 <Drawer.Screen
                     name='Home'
                     component={HomeNavigator}
@@ -232,6 +274,7 @@ const Main = () => {
                         )
                     }}
                 />
+
                 <Drawer.Screen
                     name='Directory'
                     component={DirectoryNavigator}
