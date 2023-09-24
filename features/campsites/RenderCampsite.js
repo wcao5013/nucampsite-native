ba1cefe876d0b954c7c3af33bea3f97724fab188
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-09-24 16:08:48 -0400

    message

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -47,29 +47,7 @@ const RenderCampsite = (props) => {
                 );
             }
             else if (isRightSwipe(gestureState)) {
-                //Trying to add Modal to this section, not sure
-                
-                Alert.alert(
-                    'Add Modal',
-                    'How to add modal here? ' +
-                        campsite.name +
-                        ' to comments',
-                    [
-                        {
-                            text: 'Cancel',
-                            style: 'cancel',
-                            onPress: () => console.log('Cancel Pressed')
-                        },
-                        {
-                            text: 'OK',
-                            onPress: () =>
-                                props.isFavorite
-                                    ? console.log('Already set as a favorite')
-                                    : props.markFavorite()
-                        }
-                    ],
-                    { cancelable: false }
-                );
+                props.onShowModal();
             }
         },
         
